7a0105060a206d5f55c17672a8f423c0085f81fb
syrhassan10 <hassanmahdi96.hm@gmail.com>
2020-08-10 11:21:11 -0400

    pathCreator

---
+++ Test/Assets/scripts/path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class path : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PathCreator path_c;
+    public EndOfPathInstruction end;
+    public float speed;
+    float dstTravelled;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       dstTravelled += speed * Time.deltaTime; 
+       transform.position = path_c.path.GetPointAtDistance(dstTravelled, end);
+       Quaternion target = Quaternion.Euler(180,0,-90);
+       Quaternion currentRotation= path_c.path.GetRotationAtDistance(dstTravelled, end);
+       transform.rotation = currentRotation * target;
+       Debug.Log(path_c.path.GetRotationAtDistance(dstTravelled, end).eulerAngles);
+    }
+}
