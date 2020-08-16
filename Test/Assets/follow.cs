aea34c791f76a8d4cf967fbcc4743e8b35820c1e
syrhassan10 <hassanmahdi96.hm@gmail.com>
2020-08-16 10:56:57 -0400

    Rover Moving

---
+++ Test/Assets/follow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    // Adjust the speed for the application.
+    public float speed = 1.0f;
+
+    // The target (cylinder) position.
+    public Transform target;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Move our position a step closer to the target.
+        float step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step*100);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            // Swap the position of the cylinder.
+            target.position *= -1.0f;
+        }
+    }
+}
