e894aa86c5b9f8b515a0646a623bed7394866427
syrhassan10 <hassanmahdi96.hm@gmail.com>
2020-08-27 12:03:30 -0400

    f

---
+++ CentralPark/Assets/Patrol.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public NavMeshAgent agent;
+    public GameObject[] destinations;
+    public float RemainingDistanceError;
+    public float WaitTimeAtDestination;
+    private int index = 0;
+    private float counter = 0;
+
+
+
+
+    void Start()
+    {
+     
+        agent.SetDestination(destinations[0].transform.position);
+        StartCoroutine(StartPatrol());
+    }
+    IEnumerator StartPatrol()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+            
+            if (agent.remainingDistance <= RemainingDistanceError && agent.remainingDistance!=Mathf.Infinity)
+            {
+                counter = counter + 0.01f;
+                if (counter <= WaitTimeAtDestination)
+                {
+                    agent.ResetPath();
+                    
+
+                }
+                else
+                {
+
+                    ChangeNavMeshPath();
+                    counter = 0;
+                }
+
+            }
+   
+        }
+
+     }
+
+
+    float GetVelocity()
+    {
+        return agent.speed;
+    }
+    float GetAngularVelocity()
+    {
+        return agent.angularSpeed;
+    }
+    void ChangeNavMeshPath()
+    {
+
+            incrementIndex();
+            agent.SetDestination(destinations[index].transform.position);
+  
+        
+
+    }
+
+    void incrementIndex()
+    {
+
+        if (index == destinations.Length - 1)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+    }
+}
+
+
+
+
---
+++ CentralPark/Assets/follow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using PathCreation;
+using System.Collections;
+
+public class follow : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public EndOfPathInstruction end;
+    public float speed;
+    float dstTravelled;
+    public Animator anim;
+    private bool running = true;
+    float timer;
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        
+
+    }
+    void Update()
+    {
+        if (running)
+        {
+            speed = 20.0f;
+            anim.SetInteger("state", 1);
+            speed = 20.0f;
+        }
+        else
+        {
+            speed = 0.0f;
+            anim.SetInteger("state", 0);
+            StartCoroutine(timew());
+         
+        }
+        
+        dstTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(dstTravelled, end)+ new Vector3(0,60,0);
+      
+        Quaternion rotate = pathCreator.path.GetRotationAtDistance(dstTravelled, end);
+        Quaternion fix = Quaternion.Euler(0, 0, 90);
+        transform.rotation = rotate*fix;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "chkpnt1")
+        {
+            running = false;
+            
+        }
+    }
+    IEnumerator timew()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        running = true;
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
